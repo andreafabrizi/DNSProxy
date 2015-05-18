ab1f471e50df880934770e0a80150009fbdc5871
root <superfantuz@gmail.com>
2015-05-18 08:54:40 +0200

    multithreading working !

--- dnsp2/monitor.sh
+++ dnsp2/monitor.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -x
 DNS=$1
 PORT=$2
 NUM=$3
 
 if [ -z $1 ] || [ -z $2 ] || [ -z $3 ] ; then echo "parameters missing"; exit 127; fi
 
-cat DNS.txt | head -$NUM | xargs -n 50 -I {} -P $(echo $NUM/50| bc) \
-dig -p $PORT {} @$DNS | grep -E '^[A-Za-z0-9]|Query time' | \
+#cat DNS.txt | head -$NUM | xargs -n 100 -I {} -P 1 \
+cat DNS.txt | head -$NUM | xargs -n 20 -I {} -P $(echo $NUM/20| bc) \
+dig +retry=0 -p $PORT @$DNS {} | grep -E '^[A-Za-z0-9]|Query time' | \
 awk -v xxx=$NUM 'begin{sum=0}{if (/Query time/) sum+=$4} END { print "\nAverage query time: "sum/xxx" ms"}'
 
